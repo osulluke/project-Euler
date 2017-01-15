6a4c326139ed181bed498dad09ecf0e155d68820
Luke O'Sullivan <osulluke@gmail.com>
2017-01-15 13:54:54 -0700

    Solves problem 10.

--- 10v1.c
+++ 10v1.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 
-#define MAXPRIME 1400500
+#define MAXPRIME 2000000
 #define MAXNUMBERS 200000
 
 int main(void) {
-  long primes[MAXPRIME];
+  long primes[MAXNUMBERS];
   long numPrimes = 3;
   long primeSum = 0;
   primes[0] = 2; primes[1] = 3; primes[2] = 5;
 
   for (long i = 7; i < MAXPRIME; i+=2) {
 
-    for(long j = 0; j < MAXNUMBERS; j++) {
+    for(long j = 0; primes[j] <= i; j++) {
       if (i % primes[j] == 0) {
         break;
       }
       else if (j == numPrimes - 1) {
         primes[numPrimes++] = i;
-        //printf("prime[%d] = %ld\n", j + 1, primes[j]);
       }
     }
   }
